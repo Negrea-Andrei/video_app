695bf4dedc57384c9d639e39244827ea4c385d20
Negrea-Andrei <negrea97@yahoo.com>
2024-03-18 19:17:23 +0200

    added the description checkbox

--- src/app/CreateMeeting.jsx
+++ src/app/CreateMeeting.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
 import { useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { Loader2 } from "lucide-react";
 import React from "react";
@@ -16,7 +17,34 @@ export default function CreateMeeting() {
 
   return (
     <div className="flex flex-col items-center space-y-6">
-      <h1>Welcome {user?.username}</h1>
+      <h1 className="text-center text-2xl font-bold">
+        Welcome {user?.username}
+      </h1>
+      <div className="mx-auto w-80 space-y-6 rounded-md bg-slate-100 p-5">
+        <h2 className="text-xl font-bold">Create a new meeting</h2>
+        <DescribeInputs value="value" onChange={() => ""} />
+      </div>
+    </div>
+  );
+}
+
+function DescribeInputs({ value, onChange }) {
+  const [active, setActive] = useState(false);
+
+  return (
+    <div className="space-y-2">
+      <div className="font-medium">Meeting details:</div>
+      <label className="flex items-center gap-1.5">
+        <input
+          type="checkbox"
+          checked={active}
+          onChange={(e) => {
+            setActive(e.target.checked);
+            onChange("");
+          }}
+        />
+        Add description
+      </label>
     </div>
   );
 }
