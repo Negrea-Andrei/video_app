ec91b7f462885cf3ad1df280ae796f41c8e22e2e
Andrei Negrea <negrea97@yahoo.com>
2024-03-22 09:04:43 +0200

    Update useLoadRecordings.js

--- src/app/hooks/useLoadRecordings.js
+++ src/app/hooks/useLoadRecordings.js
@@ -1,1 +1,27 @@
+import { useUser } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+
+export default function useLoadRecordings(call) {
+  const { user } = useUser();
+
+  const [recordings, setRecordings] = useState([]);
+  const [recordingsLoading, setRecordingsLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadRecordings() {
+      setRecordingsLoading(true);
+
+      if (!user || !user.id) return;
+
+      const { recordings } = await call.queryRecordings();
+      setRecordings(recordings);
+
+      setRecordingsLoading(false);
+    }
+
+    loadRecordings();
+  }, [call, user && user.id]);
+
+  return { recordings, recordingsLoading };
+}
 
