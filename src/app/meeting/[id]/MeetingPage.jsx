f82ad4b7341c074d30d6b49e3ea46a750f92e401
Negrea-Andrei <negrea97@yahoo.com>
2024-03-22 18:45:17 +0200

    create the component for the meeting screen

--- src/app/meeting/[id]/MeetingPage.jsx
+++ src/app/meeting/[id]/MeetingPage.jsx
@@ -1,44 +1,93 @@
 "use client";
 
+import { useUser } from "@clerk/nextjs";
 import {
   CallControls,
   SpeakerLayout,
   StreamCall,
   StreamTheme,
+  useCallStateHooks,
   useStreamVideoClient,
 } from "@stream-io/video-react-sdk";
+import useLoadCall from "../../hooks/useLoadCall";
 import { Loader2 } from "lucide-react";
 import { useState } from "react";
 
 export default function MeetingPage({ id }) {
-  const [call, setCall] = useState(null);
+  const { user, isLoaded: userLoaded } = useUser();
+
+  const { call, callLoading } = useLoadCall(id);
 
   const client = useStreamVideoClient();
 
-  if (!client) {
+  if (!userLoaded || callLoading) {
     return <Loader2 className="mx-auto animate-spin" />;
   }
 
   if (!call) {
+    return <p className="text-center font-bold">Call not found</p>;
+  }
+
+  const notAllowedToJoin =
+    call.type === "private-meeting" &&
+    (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+  if (notAllowedToJoin) {
     return (
-      <button
-        onClick={async () => {
-          const call = client.call("private", id);
-          await call.join();
-          setCall(call);
-        }}
-      >
-        Join
-      </button>
+      <p className="text-center font-bold">
+        You are not allowed to view this meeting
+      </p>
     );
   }
 
   return (
     <StreamCall call={call}>
       <StreamTheme className="space-y-3">
-        <SpeakerLayout />
-        <CallControls />
+        <MeetingScreen />
       </StreamTheme>
     </StreamCall>
+  );
+}
+
+function MeetingScreen() {
+  const { useCallEndedAt, useCallStartedAt } = useCallStateHooks();
+
+  const callEndedAt = useCallEndedAt();
+  const callStartsAt = useCallStartsAt();
+
+  const [setupComplete, setSetupComplete] = useState(false);
+
+  async function handleSetupComplete() {
+    call.join();
+    setSetupComplete(true);
+  }
+
+  const callIsInFuture = callStartsAt && new Date(callStartsAt) > new Date();
+
+  const callHasEnded = !!callEndedAt;
+
+  if (callHasEnded) {
+    return <MeetingEndedScreen />;
+  }
+
+  if (callIsInFuture) {
+    return <UpcomingMeetingScreen />;
+  }
+
+  const description = call.state.custom.description;
+
+  return (
+    <div className="space-y-6">
+      {description && (
+        <p className="text-center">
+          Meeting description: <span className="font-bold">{description}</span>
+        </p>
+      )}
+      {setupComplete ? (
+        <CallUI />
+      ) : (
+        <SetupUI onSetupComplete={handleSetupComplete} />
+      )}
+    </div>
   );
 }
